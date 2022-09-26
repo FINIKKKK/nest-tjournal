a60dc0c220d4d63dcce9d452ba57dc9eb50fb9fb
DIGIKRASH <digitallkrash@gmail.com>
2022-09-26 21:12:00 +0600

    jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserEntity } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByCond({
@@ -17,5 +22,14 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: UserEntity) {
+    const { password, ...userData } = user;
+    const payload = { email: user.email, sub: user.id };
+    return {
+      ...userData,
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'test',
+    });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    const data = { id: payload.sub, email: payload.email };
+
+    const { password, ...user } = await this.userService.findByCond(data);
+
+    if (!user) {
+      throw new UnauthorizedException('Нет доступа к этой страницы');
+    }
+
+    return user;
+  }
+}
